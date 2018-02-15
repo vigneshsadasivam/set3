77dfad4493d544558ac78bdb5ab712977840e3ba
vigneshsadasivam <36299168+vigneshsadasivam@users.noreply.github.com>
2018-02-15 15:23:08 +0530

    Update arithp.py

--- arithp.py
+++ arithp.py
@@ -1,16 +1,10 @@
 n=int(input("enter n value"))
-
 a=int(input("enter a value"))
-
 d=int(input("enter d value"))
-
 sum=0
 for i in range(a,n+1):
-
   sum=sum+i
-
   i=sum+d
-
 print(sum)
   
     
